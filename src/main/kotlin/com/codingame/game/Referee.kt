04ba7128cc2f0aec2b6fe55d807c58dd4860a824
Jan Kandrac <jan@kandrac.xyz>
2025-02-20 18:58:11 +0100

    Animate tile placement

--- src/main/kotlin/com/codingame/game/Referee.kt
+++ src/main/kotlin/com/codingame/game/Referee.kt
@@ -4,25 +4,27 @@ import com.codingame.gameengine.core.AbstractPlayer
 import com.codingame.gameengine.core.AbstractReferee
 import com.codingame.gameengine.core.SoloGameManager
 import com.codingame.gameengine.module.entities.GraphicEntityModule
+import com.codingame.gameengine.module.entities.Group
 import com.codingame.gameengine.module.entities.Rectangle
 import com.google.inject.Inject
 import kotlin.math.roundToInt
+import kotlin.random.Random
 
 val tileVariants = mapOf(
-    'A' to listOf(listOf("O..", "OOO"), listOf(".O", ".O", "OO"), listOf("OOO", "..O"), listOf("OO", "O.", "O."), listOf("OO", ".O", ".O"), listOf("OOO", "O.."), listOf("O.", "O.", "OO"), listOf("..O", "OOO")),
-    'B' to listOf(listOf("O.O", "OOO"), listOf("OO", ".O", "OO"), listOf("OOO", "O.O"), listOf("OO", "O.", "OO")),
-    'C' to listOf(listOf(".O.", "OOO", ".O.")),
-    'D' to listOf(listOf(".O.", "OOO"), listOf(".O", "OO", ".O"), listOf("OOO", ".O."), listOf("O.", "OO", "O.")),
-    'E' to listOf(listOf(".O.", ".O.", "OOO"), listOf("..O", "OOO", "..O"), listOf("OOO", ".O.", ".O."), listOf("O..", "OOO", "O..")),
-    'F' to listOf(listOf("OOOO"), listOf("O", "O", "O", "O")),
-    'G' to listOf(listOf("OO.", ".OO", "..O"), listOf(".OO", "OO.", "O.."), listOf("O..", "OO.", ".OO"), listOf("..O", ".OO", "OO.")),
-    'H' to listOf(listOf(".O", "OO"), listOf("OO", ".O"), listOf("OO", "O."), listOf("O.", "OO")),
-    'I' to listOf(listOf("OO", ".O", "OO", "O."), listOf("OOO.", "O.OO"), listOf(".O", "OO", "O.", "OO"), listOf("OO.O", ".OOO"), listOf("O.OO", "OOO."), listOf("O.", "OO", ".O", "OO"), listOf(".OOO", "OO.O"), listOf("OO", "O.", "OO", ".O")),
-    'J' to listOf(listOf("OOO", "..O", "..O"), listOf("OOO", "O..", "O.."), listOf("O..", "O..", "OOO"), listOf("..O", "..O", "OOO")),
-    'K' to listOf(listOf("OO", "OO")),
-    'L' to listOf(listOf(".OO", ".O.", "OO."), listOf("O..", "OOO", "..O"), listOf("..O", "OOO", "O.."), listOf("OO.", ".O.", ".OO")),
-    'M' to listOf(listOf(".OO", "OO."), listOf("O.", "OO", ".O"), listOf(".O", "OO", "O."), listOf("OO.", ".OO")),
-    'N' to listOf(listOf("OOO", "OO."), listOf("O.", "OO", "OO"), listOf(".OO", "OOO"), listOf("OO", "OO", ".O"), listOf("OO", "OO", "O."), listOf("OO.", "OOO"), listOf(".O", "OO", "OO"), listOf("OOO", ".OO"))
+    'A' to (listOf(listOf("O..", "OOO"), listOf("OO", "O.", "O."), listOf("OOO", "..O"), listOf(".O", ".O", "OO"), listOf("..O", "OOO"), listOf("O.", "O.", "OO"), listOf("OOO", "O.."), listOf("OO", ".O", ".O"))),
+    'B' to (listOf(listOf("O.O", "OOO"), listOf("OO", "O.", "OO"), listOf("OOO", "O.O"), listOf("OO", ".O", "OO"), listOf("O.O", "OOO"), listOf("OO", "O.", "OO"), listOf("OOO", "O.O"), listOf("OO", ".O", "OO"))),
+    'C' to (listOf(listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."), listOf(".O.", "OOO", ".O."))),
+    'D' to (listOf(listOf(".O.", "OOO"), listOf("O.", "OO", "O."), listOf("OOO", ".O."), listOf(".O", "OO", ".O"), listOf(".O.", "OOO"), listOf("O.", "OO", "O."), listOf("OOO", ".O."), listOf(".O", "OO", ".O"))),
+    'E' to (listOf(listOf(".O.", ".O.", "OOO"), listOf("O..", "OOO", "O.."), listOf("OOO", ".O.", ".O."), listOf("..O", "OOO", "..O"), listOf(".O.", ".O.", "OOO"), listOf("O..", "OOO", "O.."), listOf("OOO", ".O.", ".O."), listOf("..O", "OOO", "..O"))),
+    'F' to (listOf(listOf("OOOO"), listOf("O", "O", "O", "O"), listOf("OOOO"), listOf("O", "O", "O", "O"), listOf("OOOO"), listOf("O", "O", "O", "O"), listOf("OOOO"), listOf("O", "O", "O", "O"))),
+    'G' to (listOf(listOf("OO.", ".OO", "..O"), listOf("..O", ".OO", "OO."), listOf("O..", "OO.", ".OO"), listOf(".OO", "OO.", "O.."), listOf(".OO", "OO.", "O.."), listOf("OO.", ".OO", "..O"), listOf("..O", ".OO", "OO."), listOf("O..", "OO.", ".OO"))),
+    'H' to (listOf(listOf(".O", "OO"), listOf("O.", "OO"), listOf("OO", "O."), listOf("OO", ".O"), listOf("O.", "OO"), listOf("OO", "O."), listOf("OO", ".O"), listOf(".O", "OO"))),
+    'I' to (listOf(listOf("OO", ".O", "OO", "O."), listOf("OO.O", ".OOO"), listOf(".O", "OO", "O.", "OO"), listOf("OOO.", "O.OO"), listOf("OO", "O.", "OO", ".O"), listOf(".OOO", "OO.O"), listOf("O.", "OO", ".O", "OO"), listOf("O.OO", "OOO."))),
+    'J' to (listOf(listOf("OOO", "..O", "..O"), listOf("..O", "..O", "OOO"), listOf("O..", "O..", "OOO"), listOf("OOO", "O..", "O.."), listOf("OOO", "O..", "O.."), listOf("OOO", "..O", "..O"), listOf("..O", "..O", "OOO"), listOf("O..", "O..", "OOO"))),
+    'K' to (listOf(listOf("OO", "OO"), listOf("OO", "OO"), listOf("OO", "OO"), listOf("OO", "OO"), listOf("OO", "OO"), listOf("OO", "OO"), listOf("OO", "OO"), listOf("OO", "OO"))),
+    'L' to (listOf(listOf(".OO", ".O.", "OO."), listOf("O..", "OOO", "..O"), listOf(".OO", ".O.", "OO."), listOf("O..", "OOO", "..O"), listOf("OO.", ".O.", ".OO"), listOf("..O", "OOO", "O.."), listOf("OO.", ".O.", ".OO"), listOf("..O", "OOO", "O.."))),
+    'M' to (listOf(listOf(".OO", "OO."), listOf("O.", "OO", ".O"), listOf(".OO", "OO."), listOf("O.", "OO", ".O"), listOf("OO.", ".OO"), listOf(".O", "OO", "O."), listOf("OO.", ".OO"), listOf(".O", "OO", "O."))),
+    'N' to (listOf(listOf("OOO", "OO."), listOf("OO", "OO", ".O"), listOf(".OO", "OOO"), listOf("O.", "OO", "OO"), listOf("OOO", ".OO"), listOf(".O", "OO", "OO"), listOf("OO.", "OOO"), listOf("OO", "OO", "O.")))
 )
 
 val tileColors = mapOf(
@@ -50,13 +52,15 @@ class Referee : AbstractReferee() {
     @Inject
     private lateinit var graphicEntityModule: GraphicEntityModule
 
+    private lateinit var allCharacters: String
     private lateinit var remainingCharacters: String
     private lateinit var board: Array<CharArray>
 
     override fun init() {
         gameManager.firstTurnMaxTime = 2000
         gameManager.turnMaxTime = 50
-        remainingCharacters = gameManager.testCaseInput.last()
+        allCharacters = gameManager.testCaseInput.last()
+        remainingCharacters = allCharacters
         val (rows, _) = gameManager.testCaseInput.first().split(" ").map { it.toInt() }
         board = gameManager.testCaseInput.drop(1).take(rows).map { it.toCharArray() }.toTypedArray()
         initVisual()
@@ -113,23 +117,25 @@ class Referee : AbstractReferee() {
         }
     }
 
-    private fun isTaken(x: Int, y: Int) = x in board[0].indices && y in board.indices && board[y][x] != '.'
-
     private val tiles = mutableMapOf<Pair<Int,Int>, Rectangle>()
+    private val polyominos = mutableMapOf<Int, Group>()
+    private var baseX: Int = -1
+    private var baseY: Int = -1
+    private var step: Double = -1.0
 
     private fun initVisual() {
         graphicEntityModule.createSprite().setImage("background.jpg")
 
         val g = graphicEntityModule.createGroup()
-        board.indices.forEach { y -> board[0].indices.forEach { x -> graphicEntityModule.createRectangle().setFillColor(if (board[y][x] == '.') 0xFFFFFF else 0xCCDDEE).setX(x * 100 + 5).setY(y * 100 + 5).setWidth(95).setHeight(95).also { g.add(it); tiles[x to y] = it } } }
-        board[0].indices.forEach { x -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(0.0)).setX(x * 100).setY(-15).setScaleX(1.05).also { g.add(it) } }
-        board[0].indices.forEach { x -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(180.0)).setX(x * 100 + 105).setY(board.size * 100 + 20).setScaleX(1.05).also { g.add(it) } }
-        board.indices.forEach { y -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(90.0)).setX(board[0].size * 100 + 20).setY(y * 100).setScaleX(1.05).also { g.add(it) } }
-        board.indices.forEach { y -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(270.0)).setX(-15).setY(y * 100 + 105).setScaleX(1.05).also { g.add(it) } }
-        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(0.0)).setX(-15).setY(-15).also { g.add(it) }
-        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(90.0)).setX(board[0].size * 100 + 20).setY(-15).also { g.add(it) }
-        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(270.0)).setX(-15).setY(board.size * 100 + 20).also { g.add(it) }
-        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(180.0)).setX(board[0].size * 100 + 20).setY(board.size * 100 + 20).also { g.add(it) }
+        board.indices.forEach { y -> board[0].indices.forEach { x -> graphicEntityModule.createRectangle().setFillColor(if (board[y][x] == '.') 0xFFFFFF else 0xCCDDEE).setX(x * 100 + 6).setY(y * 100 + 6).setWidth(94).setHeight(94).also { g.add(it); tiles[x to y] = it } } }
+        board[0].indices.forEach { x -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(0.0)).setX(x * 100).setY(-16).setScaleX(1.06).also { g.add(it) } }
+        board[0].indices.forEach { x -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(180.0)).setX(x * 100 + 106).setY(board.size * 100 + 21).setScaleX(1.06).also { g.add(it) } }
+        board.indices.forEach { y -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(90.0)).setX(board[0].size * 100 + 21).setY(y * 100).setScaleX(1.06).also { g.add(it) } }
+        board.indices.forEach { y -> graphicEntityModule.createSprite().setImage("side_t.png").setRotation(Math.toRadians(270.0)).setX(-16).setY(y * 100 + 106).setScaleX(1.06).also { g.add(it) } }
+        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(0.0)).setX(-16).setY(-16).also { g.add(it) }
+        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(90.0)).setX(board[0].size * 100 + 21).setY(-16).also { g.add(it) }
+        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(270.0)).setX(-16).setY(board.size * 100 + 21).also { g.add(it) }
+        graphicEntityModule.createSprite().setImage("corner_tl.png").setRotation(Math.toRadians(180.0)).setX(board[0].size * 100 + 21).setY(board.size * 100 + 21).also { g.add(it) }
 
         val totalH = board.size * 100
         val totalW = board[0].size * 100
@@ -137,18 +143,39 @@ class Referee : AbstractReferee() {
         val scaleX = minOf(1.0, 1920 / totalW.toDouble())
         val scale = minOf(scaleX, scaleY) - 0.1
 
-        g.setScale(scale)
-        g.setX((1920 - (scale * totalW).roundToInt()) / 2)
-        g.setY((1080 - (scale * totalH).roundToInt()) / 2)
+        baseX = (1920 - (scale * totalW).roundToInt()) / 2
+        baseY = (1080 - (scale * totalH).roundToInt()) / 2
+        step = (scale * 100)
+        g.setScale(scale).setX(baseX).setY(baseY)
 
+        remainingCharacters.forEachIndexed { index, c ->
+            val tileGroup = graphicEntityModule.createGroup()
+            val tile = tileVariants[c] ?: throw IllegalStateException("Error 01 - please contact author via comment on contribution page")
+            val color = tileColors[c] ?: throw IllegalStateException("Error 01 - please contact author via comment on contribution page")
+            for (y in tile[0].indices) { for (x in tile[0][0].indices) { if (tile[0][y][x] != '.') tileGroup.add(graphicEntityModule.createRectangle().setWidth(100).setHeight(100).setFillColor(color).setLineColor(0x000000).setLineWidth(3.0).setX(x * 100).setY(y * 100)) } }
+            polyominos[index] = tileGroup.setX(50 + Random.nextInt(150)).setY(450 + Random.nextInt(100)).setZIndex(100 - index).setScale(scale).setRotation(Math.random() * 2 * Math.PI)
+        }
     }
 
     private fun visualize(tile: List<String>, row: Int, col: Int) {
-        for (dy in tile.indices) {
-            for (dx in tile[0].indices) {
-                if (tile[dy][dx] == '.') continue
-                tiles[col + dx to row + dy]?.setFillColor(tileColors[remainingCharacters[0]]!!)
-            }
-        }
+        val polyomino = polyominos[allCharacters.indexOf(remainingCharacters[0])] ?: throw IllegalStateException("Error 02 - please contact author via comment on contribution page")
+        val configuration = tileVariants[remainingCharacters[0]]?.indexOf(tile) ?: throw IllegalStateException("Error 03 - please contact author via comment on contribution page")
+        val flip = configuration / 4 == 1
+        val rightRotations = configuration % 4
+
+        var dx = if (rightRotations in 1..2) tile[0].length else 0
+        var dy = if (rightRotations in 2..3 && !flip) tile.size else 0
+
+        if (flip && rightRotations == 0) dx += tile[0].length
+        if (flip && rightRotations == 1) dy += tile.size
+        if (flip && rightRotations == 2) { dx -= tile[0].length; dy += tile.size }
+
+        polyomino
+            .setScaleX(polyomino.scaleX * if (flip) -1.0 else 1.0)
+            .setRotation(Math.toRadians(90.0 * rightRotations))
+            .setX(3 + baseX + ((col + dx) * step).roundToInt())
+            .setY(3 + baseY + ((row + dy) * step).roundToInt())
+        graphicEntityModule.commitWorldState(0.99)
+        polyomino.setZIndex(1)
     }
 }
--- src/test/java/TestPlayer.java
+++ src/test/java/TestPlayer.java
@@ -3,53 +3,23 @@ import java.util.Scanner;
 
 public class TestPlayer {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int turn = 1;
-        while (true) {
-            String remainingTiles = scanner.nextLine();
-            String currentTile = scanner.nextLine();
-
-            System.err.println(remainingTiles);
-            System.err.println(currentTile);
-
-            int h = scanner.nextInt();
-            int w = scanner.nextInt();
-            System.err.println(h + " " + w);
-            scanner.nextLine();
-            ArrayList<String> map = new ArrayList<>();
-            for (int i = 0; i < h; i++) {
-                map.add(scanner.nextLine());
-                System.err.println(map.get(map.size() - 1));
-            }
-            if (turn == 1) {
-                System.out.println("(2,4) (3,4) (4,3) (4,4)");
-            } else if (turn == 2) {
-                System.out.println("(2,2) (3,1) (3,2) (3,3) (4,2)");
-            } else if (turn == 3) {
-                System.out.println("(0,7) (0,8) (1,6) (1,7) (1,8)");
-            } else if (turn == 4) {
-                System.out.println("(4,5) (4,6) (5,4) (5,5) (6,4)");
-            } else if (turn == 5){
-                System.out.println("(2,8) (3,7) (3,8) (4,7) (5,7) (5,8)");
-            } else if (turn == 6) {
-                System.out.println("(6,0) (6,1) (7,0) (8,0) (8,1)");
-            } else if (turn == 7){
-                System.out.println("(4,1) (5,0) (5,1)");
-            } else if (turn == 8){
-                System.out.println("(7,7) (7,8) (8,7) (8,8)");
-            } else if (turn == 9){
-                System.out.println("(5,6) (6,6) (6,7) (6,8) (7,6)");
-            } else if (turn == 10){
-                System.out.println("(0,0) (1,0) (2,0) (3,0)");
-            } else if (turn == 11){
-                System.out.println("(2,6) (2,7) (3,5) (3,6)");
-            } else if (turn == 12){
-                System.out.println("(0,1) (1,1) (1,2) (2,1)");
-            } else if (turn == 13){
-                System.out.println("(5,2) (5,3) (6,2) (7,1) (7,2)");
-            }
-            turn++;
-        }
+        System.out.println("(8,3) (9,3) (10,2) (10,3) (10,4)");
+        System.out.println("(0,2) (0,3) (1,2) (2,1) (2,2)");
+        System.out.println("(3,2) (3,3) (3,4) (4,4) (5,4)");
+        System.out.println("(2,6) (3,5) (3,6) (4,5)");
+        System.out.println("(0,4) (0,5) (0,6) (1,4)");
+        System.out.println("(0,0) (0,1) (1,0) (1,1) (2,0)");
+        System.out.println("(3,0) (3,1) (4,1) (4,2) (5,2)");
+        System.out.println("(1,3) (1,5) (1,6) (2,3) (2,4) (2,5)");
+        System.out.println("(4,3) (5,3) (6,3) (7,3)");
+//        System.out.println("(8,3) (9,3) (10,2) (10,3) (10,4)");
+//        System.out.println("(0,2) (0,3) (1,2) (2,1) (2,2)");
+//        System.out.println("(3,2) (3,3) (3,4) (4,4) (5,4)");
+//        System.out.println("(2,6) (3,5) (3,6) (4,5)");
+//        System.out.println("(1,5) (2,3) (2,4) (2,5)");
+//        System.out.println("(0,0) (0,1) (1,0) (1,1) (2,0)");
+//        System.out.println("(3,0) (3,1) (4,1) (4,2) (5,2)");
+//        System.out.println("(0,4) (0,5) (0,6) (1,3) (1,4) (1,6)");
+//        System.out.println("(4,3) (5,3) (6,3) (7,3)");
     }
 }
